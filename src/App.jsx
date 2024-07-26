85b2565a1e6520504368e7ea1c12f71a8f06c37f
yeahbudd01 <billcorcoro@comcast.net>
2024-07-26 10:17:22 -0700

    Set up basic frame work for frontend and created components for an example first page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,55 @@
 import { useState } from 'react'
 
+//Creating a sample card for character data first before bringing in the data
+//from the backend to deal with logic first to see if everything works from the front end.
+const sampleCard = [
+  {
+    Player_Name: "Bill C.",
+    Character_Name: "Busterfield Sharpgrin",
+    Class: "Warlock",
+    Race: "Half-Orc",
+    Level: 6,
+    Health_Points: 53,
+    Alive: true,
+    Initiative: 12
+  },
+  {
+    Player_Name: "Bill C.",
+    Character_Name: "Ardane Rackland",
+    Class: "Blood-Hunter",
+    Race: "Human",
+    Level: 10,
+    Health_Points: 105,
+    Alive: false,
+    Initiative: 15
+  },
+  {
+    Player_Name: "Bill C.",
+    Character_Name: "Zekun Protector",
+    Class: "Cleric",
+    Race: "Warforged",
+    Level: 4,
+    Health_Points: 54,
+    Alive: true,
+    Initiative: 18
+  }
+]
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
-      <h1>Vite + React</h1>
+      <h1>Welcome to the Select Card Page</h1>
       <div className="card">
         <button onClick={() => setCount((prevCount) => prevCount + 1)}>
-          count is {count}
+          count is {count} I'm keeping this here for state example
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        </div>
     </>
+
+
+
   )
 }
 
---
+++ src/components/EverythingCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function EverythingCard() {
+  return (
+    <div>EverythingCard</div>
+  )
+}
+
+export default EverythingCard
