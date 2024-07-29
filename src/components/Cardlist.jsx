8d42c9a6d6f0fc754320f727a6c2a4da102e4572
yeahbudd01 <billcorcoro@comcast.net>
2024-07-28 21:27:20 -0700

    Fixed an issue with fragments and keys not showing up.

--- src/components/Cardlist.jsx
+++ src/components/Cardlist.jsx
@@ -6,10 +6,7 @@ export default function Cardlist({ characardClass }) {
     //using a .map method to cycle through the array of objects to create a new array of SelectCards with character data passed through from
     <div className="card-grid">
         {characardClass.map(charaCard =>{
-            return <>
-            <SelectCard character={charaCard} key={charaCard._id} />
-            <br></br>
-            </>
+            return <SelectCard character={charaCard} key={charaCard._id} />
         })}
     </div>
   )
--- src/components/SelectCard.jsx
+++ src/components/SelectCard.jsx
@@ -2,14 +2,9 @@ import React from 'react'
 
 export default function SelectCard({ character }) {
 
-  function handleChange(e){
-    console.log(e)
-  }
-
   return (
     <>
     <div className='select-card' >
-      <input type='checkbox' onChange={handleChange}/>
       <div>{character.Character_Name}</div>
       <div>{character.Class}</div>
       <div>Level: {character.Level}</div>
