1d0fcc43f0f730b8be796232c9651d5ea37b3feb
yeahbudd01 <billcorcoro@comcast.net>
2024-07-29 01:11:35 -0700

    Added delete data capabilities on the Delete page.

--- src/components/CardDelete.jsx
+++ src/components/CardDelete.jsx
@@ -1,6 +1,19 @@
 import React from 'react'
+import axios from 'axios'
 //Card component which is for rendering character cards on the main page called Card Display. 
 export default function Card({ character }) {
+
+    async function handleSubmit(e){
+        e.preventDefault()
+       try {
+          let res = await axios({
+            method: 'DELETE',
+            url: `http://localhost:4000/characters/${character._id}`,
+          })
+        } catch (err) {
+          console.error(err)
+        }
+      }
 
   return (
     <>
@@ -10,7 +23,7 @@ export default function Card({ character }) {
       <div>Level: {character.Level}</div>
       <div>HP: {character.Health_Points}</div>
       <div>Player Name: {character.Player_Name}</div>
-      <button type='submit'>Delete this Character</button>
+      <button onClick={handleSubmit} >Delete this Character</button>
     </div>
     </>
   )
